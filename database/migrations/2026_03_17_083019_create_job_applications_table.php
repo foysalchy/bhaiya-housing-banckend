f42d567d367d3391627f66df18e19367d05bc348
Rojifa-Akhter <mstrojifaakhter@gmail.com>
2026-03-17 15:12:38 +0600

    career

---
+++ database/migrations/2026_03_17_083019_create_job_applications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('content_id')->constrained('contents')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('subject');
+            $table->string('resume');
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
