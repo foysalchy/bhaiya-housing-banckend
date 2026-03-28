da77d74a112f6f9be32f33fcd1d6b5fa5c06197b
kalam714 <kalamahmed714@gmail.com>
2026-03-28 11:37:31 +0600

    chnages

--- routes/web.php
+++ routes/web.php
@@ -43,12 +43,13 @@ Route::post('/career/apply', [WebController::class, 'applyJob'])->name('job.appl
 Route::get('/projects', [WebController::class, 'projects'])->name('projects');
 Route::get('/projects/{id}', [WebController::class, 'showProject'])->name('projects.show');
 Route::get('/events', [WebController::class, 'events'])->name('events');
-Route::get('/news/{id}',   [WebController::class, 'show'])->name('news.show');
-Route::get('/events/{id}', [WebController::class, 'show'])->name('events.show');
+
 Route::get('/customer-contact',        [WebController::class, 'customerContact'])->name('contact');
 Route::post('/customer-contact/store', [WebController::class, 'customerContactStore'])->name('contact.store');
 Route::get('/landowner-contact',        [WebController::class, 'landowner'])->name('landowner');
 Route::post('/landowner-contact/store', [WebController::class, 'landownerStore'])->name('landowner.store');
 Route::get('/concerns', [WebController::class, 'concerns'])->name('concerns');
+Route::get('/news/{id}',   [WebController::class, 'show'])->name('news.show');
+Route::get('/events/{id}', [WebController::class, 'show'])->name('events.show');
 
 
